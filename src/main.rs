dcb421b6e7c4af37beede520cc4ac55b0899dff1
Dmytro Kolosovskyi <kolosovskyi.dmytro@gmail.com>
2024-04-10 00:29:25 +0200

    used the neural network for XOR prediction

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
+use lib::{network::Network, activations::SIGMOID};
+
 pub mod lib;
 
+// XOR prediction
 fn main() {
-    println!("Hello, world!");
+    let inputs = vec![
+		vec![0.0, 0.0],
+		vec![0.0, 1.0],
+		vec![1.0, 0.0],
+		vec![1.0, 1.0],
+	];
+	let targets = vec![vec![0.0], vec![1.0], vec![1.0], vec![0.0]];
+
+	let mut network = Network::new(vec![2, 3, 1], 0.5, SIGMOID);
+
+	network.train(inputs, targets, 10000);
+
+	println!("0 and 0: {:?}", network.feed_forward(vec![0.0, 0.0]));
+	println!("0 and 1: {:?}", network.feed_forward(vec![0.0, 1.0]));
+	println!("1 and 0: {:?}", network.feed_forward(vec![1.0, 0.0]));
+	println!("1 and 1: {:?}", network.feed_forward(vec![1.0, 1.0]));
 }
